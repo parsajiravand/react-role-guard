174078e1927c34636b6f6d232407dd2b566dcf3d
parsajiravand <parsajiravand@gmail.com>
2026-03-23 20:44:14 +0330

    Update react version

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      'advanced-react-role-guard': path.resolve(__dirname, '../src/index.ts'),
-    },
+    dedupe: ['react', 'react-dom'],
   },
   server: {
     port: 5174,
